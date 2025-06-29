083cafc71fd882ea2ab41a61cffb2110b57fa1af
Shashi Kumar <kushwahshashi.dev@gmail.com>
2025-06-29 09:02:35 +0000

    Lecture 1: Basic Shell scripts - Hello World and Sys Info

---
+++ stage1-intro/lecture-01/hello_world.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Hello, $USER! Welcome to Shell scripting."
+echo "Today is $(date)"
+echo "You are in $(pwd)"
+
---
+++ stage1-intro/lecture-01/sysinfo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Hostname: $(hostname)"
+echo "Date: $(date)"
+echo "Disk Usage:"
+df -h
+
